3ca79c0c1f02422b632b8f64f8c5360ce4d076e0
Zane Bliss <zanebliss@icloud.com>
2024-06-30 20:28:55 -0500

    add sanity check with rspec test

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,6 +4,25 @@ use std::{
 };
 
 #[test]
+fn sanity_check_with_rspec() -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new("tests/fixtures/sanity_check_with_rspec");
+    set_current_dir(path)?;
+
+    let mut cmd = assert_cmd::Command::cargo_bin("teva")?;
+
+    insta::with_settings!({ filters => vec![
+        (r"\b\d*\.\d+\b", "[TIME]"),
+        (r"33m[a-z0-9]{7}", "[SHA]")
+    ]}, {
+        insta::assert_snapshot!(String::from_utf8(cmd.output().unwrap().stdout).unwrap());
+    });
+
+    set_current_dir(Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()))?;
+
+    Ok(())
+}
+
+#[test]
 fn not_enough_commits() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new("tests/fixtures/not_enough_commits");
     set_current_dir(path)?;
